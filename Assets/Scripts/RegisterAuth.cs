9a1c68f898f8c9e8e6444cf9391a8e8c6748b384
Cesar-Augusto-Cruz-Andrade <cesarrock02@hotmail.com>
2022-11-15 13:19:59 -0300

    Revert "add tasks2"
    This reverts commit 6a9202e13f45c6fe98214107dac8b9292ec3f674.

--- Assets/Scripts/RegisterAuth.cs
+++ Assets/Scripts/RegisterAuth.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using Firebase;
 using Firebase.Auth;
 using TMPro;
+using Firebase.Firestore;
+
 
 
 public class RegisterAuth : MonoBehaviour
@@ -76,17 +78,17 @@ public class RegisterAuth : MonoBehaviour
         switch (errorCode)
         {
             case AuthError.MissingEmail:
-                return "E-mail não preenchido";
+                return "E-mail nï¿½o preenchido";
             case AuthError.MissingPassword:
                 return "Por favor insira a senha";
             case AuthError.WeakPassword:
                 return "Senha fraca";
             case AuthError.InvalidEmail:
-                return "E-mail inválido";
+                return "E-mail invï¿½lido";
             case AuthError.EmailAlreadyInUse:
-                return "Este e-mail já está cadastrado";
+                return "Este e-mail jï¿½ estï¿½ cadastrado";
             default:
-                return "Erro não especificado!";
+                return "Erro nï¿½o especificado!";
         }
     }
 
@@ -103,8 +105,14 @@ public class RegisterAuth : MonoBehaviour
                 HandleProfileCreationErrors(ProfileTask.Exception);
             }else{
                 Debug.Log("User set successfully");
-                LogMessage("Usuário criado. confirmação de e-mail pendente. Por favor, verifique sua caixa de entrada e confirme o e-mail.");
+                LogMessage("Usuï¿½rio criado. confirmaï¿½ï¿½o de e-mail pendente. Por favor, verifique sua caixa de entrada e confirme o e-mail.");
                 SendEmailToUser(FirebaseAuthenticator.instance.User);
+                Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+                var _userData = new UserData{
+                    Tasks =  0
+                };
+                var Firestore = FirebaseFirestore.DefaultInstance;
+                Firestore.Collection(path:"users_sheet").Document(path:auth.CurrentUser.UserId.ToString()).SetAsync(_userData);
             }
         }
 
@@ -132,7 +140,7 @@ public class RegisterAuth : MonoBehaviour
              Debug.LogWarning(message: $"Failed to register taks with {profileException}");
              FirebaseException firebaseEx = profileException.GetBaseException() as FirebaseException;
              AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
-             LogMessage("Falha ao registrar usuário", errorColor);
+             LogMessage("Falha ao registrar usuï¿½rio", errorColor);
         }
     }
 }

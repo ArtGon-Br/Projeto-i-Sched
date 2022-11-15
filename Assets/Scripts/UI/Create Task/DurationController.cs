1ba59b94c874e7b678920a084d98f951abf73c56
Cesar-Augusto-Cruz-Andrade <cesarrock02@hotmail.com>
2022-11-15 12:01:43 -0300

    add tasks

--- Assets/Scripts/UI/Create Task/DurationController.cs
+++ Assets/Scripts/UI/Create Task/DurationController.cs
@@ -8,10 +8,10 @@ public class DurationController : MonoBehaviour
     [SerializeField] TMP_InputField hourInput;
     [SerializeField] TMP_InputField minInput;
 
-    private void OnEnable()
+    private void Awake()
     {
-        hourInput.SetTextWithoutNotify("00");
-        minInput.SetTextWithoutNotify("00");
+        /*hourInput.SetTextWithoutNotify("00");
+        minInput.SetTextWithoutNotify("00");*/
 
         hourInput.onEndEdit.AddListener(x =>
         {
